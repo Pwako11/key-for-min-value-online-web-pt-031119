d088f5f0fb59acac01b4cad151cdac7944d9c5a3
Patrick A <akolo@hotmail.com>
2019-03-23 22:20:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,8 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       array << value
     end 
-    
-  name_hash.group_by{|key, value| value}.min_by{|key, value| key}.last.to_h
-  min_val = key
+    array.sort = min_val
+    name_hash.each do |key, value|
+      if min_val[0] == value
+        return key
 end
