761bda08eeb8b5628dd1f3c3bfde4d164792c10d
Patrick A <akolo@hotmail.com>
2019-03-23 21:49:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,6 @@
 
 def key_for_min_value(name_hash)
   name_hash = {:blake => 500, :ashley => 2, :adam => 1}
-  name_hash.min_by{|key, value| value} value.min_val
+  name_hash.group_by{|key, value| value}.min_by{|key, value| key}.last.to_h
   min_val = key
 end
