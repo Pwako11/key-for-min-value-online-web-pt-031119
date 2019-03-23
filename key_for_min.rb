b6200306b29f02474d0bb72015186d140ab7dda1
Patrick A <akolo@hotmail.com>
2019-03-23 21:43:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash = {:blake => 500, :ashley => 2, :adam => 1}
   name_hash.select {|key, value| key.min_val}
   min_val = key
 end
