54269901abe851de2e587d962a7561beb2d88fae
Patrick A <akolo@hotmail.com>
2019-03-23 21:43:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,6 @@
 
 def key_for_min_value(name_hash)
   name_hash = {:blake => 500, :ashley => 2, :adam => 1}
-  name_hash.select {|key, value| key.min_val}
+  name_hash.select {|key, value| value.min_val}
   min_val = key
 end
